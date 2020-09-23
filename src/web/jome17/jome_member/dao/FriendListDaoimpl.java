9f1a968d1ea798cb3d7631592d4cbfca95c91943
sam_test <>
2020-09-23 17:58:18 +0800

    clear

--- src/web/jome17/jome_member/dao/FriendListDaoimpl.java
+++ src/web/jome17/jome_member/dao/FriendListDaoimpl.java
@@ -127,8 +127,7 @@ public class FriendListDaoimpl implements CommonDao<FriendListBean, String> {
 		return friendList;
 	}
 	
-<<<<<<< HEAD
-=======
+
 	
 	//查出memberId的朋友列表
 	@Override
@@ -197,18 +196,14 @@ public class FriendListDaoimpl implements CommonDao<FriendListBean, String> {
 				}
 				return null;
 	}
->>>>>>> c96d6483bce07be1c11488859c98f267792b0117
+
 
 
 /*
  * 以下為目前不需要實作的方法
  */
 	
-	@Override
-	public List<FriendListBean> selectAll(String memberId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 	
 	@Override
 	public byte[] getImage(String acconut) {
